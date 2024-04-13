0bc5ce04aaaccca097b3d89f9328476706674750
Standa Novák <info@stanov.cz>
2024-04-13 16:03:28 +0200

    Ducklings and walls

--- Ludum Dare 55/Assets/DucklingMovement.cs
+++ Ludum Dare 55/Assets/DucklingMovement.cs
@@ -14,7 +14,9 @@ public class DucklingMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        _rigidbody.MoveRotation(_rigidbody.rotation + 1);
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.angularVelocity = 0;
+        _rigidbody.SetRotation(_rigidbody.rotation + 1);
         _rigidbody.MovePosition(_rigidbody.position + new Vector2(transform.up.x, transform.up.y) * Time.fixedDeltaTime);
     }
 }
