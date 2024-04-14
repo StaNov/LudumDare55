d55fb040d444951b151a5a4df936fdbfa59d9d51
Standa Novák <info@stanov.cz>
2024-04-14 12:07:59 +0200

    Meteorite hits

--- Ludum Dare 55/Assets/DucklingHit.cs
+++ Ludum Dare 55/Assets/DucklingHit.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DucklingCarHit : MonoBehaviour
+public class DucklingHit : MonoBehaviour
 {
     public Sprite splashSprite;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.name.StartsWith("Car"))
-            return;
+        if (other.name.StartsWith("Car"))
+            OnHit();
+    }
 
+    public void OnHit()
+    {
         GetComponent<DucklingMovement>().enabled = false;
         GetComponent<Rigidbody2D>().isKinematic = true;
         GetComponent<SpriteRenderer>().sprite = splashSprite;
--- Ludum Dare 55/Assets/Meteorite.cs
+++ Ludum Dare 55/Assets/Meteorite.cs
@@ -1,44 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Meteorite : MonoBehaviour
 {
     public Sprite meteoriteSprite;
     private Sprite emptySprite;
     private float shadowEnlargingTime;
-    private const float meteoriteDespawnTime = 1f;
+    private const float meteoriteDespawnTime = 0.2f;
     private SpriteRenderer _renderer;
+    private CircleCollider2D _collider;
+
+    private List<GameObject> currentlyColliding = new();
+    
     IEnumerator Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<CircleCollider2D>();
         emptySprite = _renderer.sprite;
         shadowEnlargingTime = Random.Range(2f, 4f);
         var originalSpriteColor = _renderer.color;
+        var loopStartTime = 0f;
 
         while (true)
         {
             SetScale(0);
-            var loopStartTime = Time.time;
+            loopStartTime = loopStartTime != 0 ? Time.time : Time.time - Random.Range(0, shadowEnlargingTime);
             _renderer.sprite = emptySprite;
             _renderer.color = originalSpriteColor;
-            
+            _collider.isTrigger = true;
 
             while (Time.time - loopStartTime < shadowEnlargingTime)
             {
-                SetScale((Time.time - loopStartTime) / shadowEnlargingTime);
+                var scale = (Time.time - loopStartTime) / shadowEnlargingTime;
+                SetScale(scale * scale);
                 yield return null;
             }
 
             _renderer.sprite = meteoriteSprite;
             _renderer.color = Color.white;
-            yield return new WaitForSeconds(0.5f);
+
+            foreach (var colliding in new List<GameObject>(currentlyColliding))
+            {
+                var hit = colliding.GetComponent<DucklingHit>();
+
+                if (hit != null)
+                {
+                    hit.OnHit();
+                }
+            }
+
+            _collider.isTrigger = false;
+            
+            yield return new WaitForSeconds(1.5f);
 
             var meteoriteDespawnStartTime = Time.time;
 
             while (Time.time - meteoriteDespawnStartTime < meteoriteDespawnTime)
             {
-                SetAlpha((1 - (Time.time - meteoriteDespawnStartTime)) / meteoriteDespawnTime);
+                SetAlpha(1 - (Time.time - meteoriteDespawnStartTime) / meteoriteDespawnTime);
                 yield return null;
             }
         }
@@ -57,5 +79,15 @@ public class Meteorite : MonoBehaviour
         newScale.x = scale;
         newScale.y = scale;
         transform.localScale = newScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        currentlyColliding.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        currentlyColliding.Remove(other.gameObject);
     }
 }
