a4bec4c1dd4c0b140406cf6c1b8d899e62ccefd6
Roman Plevka <rplevka@redhat.com>
2024-04-15 23:00:06 +0200

    variable-color cars

--- Ludum Dare 55/Assets/CarMovement.cs
+++ Ludum Dare 55/Assets/CarMovement.cs
@@ -6,15 +6,20 @@ public class CarMovement : MonoBehaviour
 {
     public bool rightToLeft;
     public Sprite[] carSprites;
+    public Sprite[] carPaintSprites;
     
+    private int _spriteIndex;
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _renderer;
+    private SpriteRenderer _rendererPaint;
     private Vector3 _initPosition;
+
     
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _renderer = GetComponent<SpriteRenderer>();
+        _rendererPaint = GetComponentsInChildren<SpriteRenderer>()[1];
         _initPosition = transform.position;
     }
     
@@ -22,7 +27,11 @@ public class CarMovement : MonoBehaviour
     {
         while (true)
         {
-            _renderer.sprite = carSprites[Random.Range(0, carSprites.Length)];
+            _spriteIndex = Random.Range(0, carSprites.Length);
+            _renderer.sprite = carSprites[_spriteIndex];
+            _rendererPaint.sprite = carPaintSprites[_spriteIndex];
+
+            _rendererPaint.color = new Color(Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
             transform.position = _initPosition;
             _rigidbody.velocity = Vector2.right * Random.Range(6, 9) * (rightToLeft ? -1 : 1);
 
